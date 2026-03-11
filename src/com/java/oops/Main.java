f239c6cb3001207840d5e79e53d8d25830372561
pranay8873 <pranaypuvaati@gmail.com>
2026-03-12 00:34:20 +0530

    added customer class and obj

--- src/com/java/oops/Main.java
+++ src/com/java/oops/Main.java
@@ -4,6 +4,21 @@ package com.java.oops;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        customer cu=new customer();
+        System.out.println("Name : "+cu.name);
+        System.out.println("Age : "+cu.age);
+        System.out.println("phone number : "+cu.phoneNo);
+        System.out.println("Email : "+cu.email);
+        System.out.println("customer Id : "+cu.cos_Id);
+        System.out.println("Gender : "+cu.gender);
+        System.out.println("Region : "+cu.region);
+        customer cu2=new customer("pranay","8919836703",20,"M",6699,"pranaypuvvati@gmail.com","India");
+        System.out.println("Name : "+cu2.name);
+        System.out.println("Age : "+cu2.age);
+        System.out.println("phone number : "+cu2.phoneNo);
+        System.out.println("Email : "+cu2.email);
+        System.out.println("customer Id : "+cu2.cos_Id);
+        System.out.println("Gender : "+cu2.gender);
+        System.out.println("Region : "+cu2.region);
     }
 }
--- src/com/java/oops/customer.java
+++ src/com/java/oops/customer.java
@@ -2,9 +2,30 @@ package com.java.oops;
 
 public class customer {
     String name;
-    double phoneNo;
-    byte age;
-    char gender;
-    
+    String phoneNo;
+    int age;
+    String gender;
+    int cos_Id;
+    String email;
+    String region;
+    customer(){
+        this.name="unknown";
+        this.phoneNo="0";
+        this.age=0;
+        this.gender="-";
+        this.cos_Id=0;
+        this.email="Unknown";
+        this.region="Unknown";
+    }
+    customer(String name,String phoneNo,int age,String gender,int cos_Id,String email,String region){
+        this.name=name;
+        this.phoneNo=phoneNo;
+        this.age=age;
+        this.gender=gender;
+        this.cos_Id=cos_Id;
+        this.email=email;
+        this.region=region;
+    }
 
 }
+
