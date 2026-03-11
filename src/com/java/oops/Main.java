d1ac1a5840eeafb205b0d76e69d1f103c7667dc1
pranay8873 <pranaypuvaati@gmail.com>
2026-03-11 23:48:11 +0530

    created new repository for oops in java

---
+++ src/com/java/oops/Main.java
@@ -0,0 +1,9 @@
+package com.java.oops;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/java/oops/customer.java
@@ -0,0 +1,10 @@
+package com.java.oops;
+
+public class customer {
+    String name;
+    double phoneNo;
+    byte age;
+    char gender;
+    
+
+}
